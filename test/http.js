3291329394455bae618e33fd9904b4bbb9332151
st-sloth <stsloth@protonmail.com>
2018-07-15 00:41:35 +0500

    es5ify for old nodes

--- test/http.js
+++ test/http.js
@@ -1,12 +1,12 @@
-const http = require('http')
-const request = require('supertest')
+var http = require('http')
+var request = require('supertest')
 
 /**
  * Request `/<responseStatusCode>`
  */
 function createServer() {
     return http.createServer(function onRequest(req, res) {
-        const reqEndpoint = Number(req.url.match(/\/(\d*)/)[1])
+        var reqEndpoint = Number(req.url.match(/\/(\d*)/)[1])
 
         if (100 <= reqEndpoint && reqEndpoint < 600) {
             res.statusCode = reqEndpoint
@@ -21,19 +21,19 @@ const server = createServer()
 
 
 describe('http', function () {
-    for (let hundreds = 1; hundreds < 6; ++hundreds) {
-        for (let i = 0; i <= 10; ++i) {
-            const status = hundreds * 100 + i
-
-            it(String(status), function (done) {
-                request(server)
-                    .get('/' + status)
-                    .expect(status)
-                    .end((err, res) => {
-                        if (err) throw err
-                        done()
-                    })
-            })
+    for (var hundreds = 1; hundreds < 6; ++hundreds) {
+        for (var i = 0; i <= 10; ++i) {
+            (function (status) {
+                it(String(status), function (done) {
+                    request(server)
+                        .get('/' + status)
+                        .expect(status)
+                        .end(function (err, res) {
+                            if (err) throw err
+                            done()
+                        })
+                })
+            })(hundreds * 100 + i)
         }
     }
 })
